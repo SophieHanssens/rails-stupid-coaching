c053102f5993c439822554be4036d8f8a6a3d90c
Sophie Hanssens <sophie.hanssens@gmail.com>
2021-12-06 14:15:54 +0100

    style, première passe

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 class QuestionsController < ApplicationController
-  def ask
-  end
+  def ask; end
+
   def answer
-     # @question = []
+    # @question = []
     @answer = if params[:question] == 'I am going to work'
-      'Great!'
-    elsif params[:question].include?("?")
-      'Silly question, get dressed and go to work!'
-    else
-      'I don/t care, get dressed and go to work!'
-    end
+                'Great!'
+              elsif params[:question].include?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                'I don/t care, get dressed and go to work!'
+              end
   end
 end
